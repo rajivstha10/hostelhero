0211d6d59f3f025ac782c7765ba7aacdac5cedf1
rajivstha10 <rajivstha10@gmail.com>
2023-09-08 12:20:35 +0545

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hostelhero/features/logs/splash_screen.dart';
+import 'package:hostelhero/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
